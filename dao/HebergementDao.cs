54857d3c2675341cb3ac4f14204861c92640432b
gaelle ducrocq <ducrocqgaelle@gmail.com>
2020-07-08 11:36:43 +0200

    Création HébergementDao

---
+++ dao/HebergementDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiteHouse.dao
+{
+    public class HebergementDao
+    {
+        private GiteHouseEntities db = new GiteHouseEntities();
+
+        // GET: Hebergements
+        public List<Hebergement> GetHebergements()
+        {
+            var hebergements = db.Hebergements.Include(h => h.Adresse).Include(h => h.Utilisateur).Include(h => h.TypeHebergement);
+            return View(hebergements.ToList());
+        }
+    }
+}

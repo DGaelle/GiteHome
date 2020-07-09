53213aed04171e845b75dbff37d049433f15601e
gaelle ducrocq <ducrocqgaelle@gmail.com>
2020-07-09 16:41:10 +0200

    add messagecontroller + liens navbar

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GiteHouse;
+
+namespace GiteHouse.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private GiteHouseEntities db = new GiteHouseEntities();
+
+        // GET: Messages
+        public ActionResult Index()
+        {
+            var messages = db.Messages.Include(m => m.Conversation);
+            return View(messages.ToList());
+        }
+
+        // GET: Messages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdConversation = new SelectList(db.Conversations, "IdConversation", "Titre");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdMessage,IdConversation,IdExpediteur,IdDestinataire,Texte,Date,IsLu")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdConversation = new SelectList(db.Conversations, "IdConversation", "Titre", message.IdConversation);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdConversation = new SelectList(db.Conversations, "IdConversation", "Titre", message.IdConversation);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdMessage,IdConversation,IdExpediteur,IdDestinataire,Texte,Date,IsLu")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(message).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdConversation = new SelectList(db.Conversations, "IdConversation", "Titre", message.IdConversation);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = db.Messages.Find(id);
+            if (message == null)
+            {
+                return HttpNotFound();
+            }
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Message message = db.Messages.Find(id);
+            db.Messages.Remove(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
